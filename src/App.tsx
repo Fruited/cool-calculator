8babdbccb79ecea05ecf95a3fd086ff34f8ce587
Steven Hernström <steven.hernstrom@Steven-Hernstrom-Mac.local>
2024-10-17 11:10:43 +0200

    removed the template react app, added buttons and simple styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="calculator">
+        <div>
+          <input type="text" readOnly />
+        </div>
+        <div>
+          <button className="basic-button">c</button>
+          <button className="basic-button">d</button>
+          <button className="basic-button">.</button>
+          <button className="basic-button">/</button>
+        </div>
+        <div>
+          <button className="basic-button">7</button>
+          <button className="basic-button">8</button>
+          <button className="basic-button">9</button>
+          <button className="basic-button">*</button>
+        </div>
+        <div>
+          <button className="basic-button">4</button>
+          <button className="basic-button">5</button>
+          <button className="basic-button">6</button>
+          <button className="basic-button">-</button>
+        </div>
+        <div>
+          <button className="basic-button">1</button>
+          <button className="basic-button">2</button>
+          <button className="basic-button">3</button>
+          <button className="basic-button">+</button>
+        </div>
+        <div>
+          <button className="basic-button">0</button>
+          <button className="basic-button">00</button>
+          <button className="basic-button">=</button>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
