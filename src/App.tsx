ade09be84186c4ffcd27a0638fecf68b5ccec00b
Steven Hernström <tstevenlh@gmail.com>
2024-10-18 17:12:35 +0200

    Added functions for operators and clear/delete

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,36 @@ import "./App.css";
 function App() {
   const [currentValue, setCurrentValue] = useState("0");
   const [previousValue, setPreviousValue] = useState("");
+  const [currentOperator, setCurrentOperator] = useState("");
 
   function onNumberClick(value: string) {
     if (currentValue === "0") {
       setCurrentValue(value);
     } else {
       setCurrentValue(currentValue + value);
+    }
+  }
+
+  function onOperatorClick(operator: string) {
+    setCurrentOperator(operator);
+    setPreviousValue(currentValue);
+    setCurrentValue("");
+  }
+
+  function clear() {
+    setCurrentValue("0");
+    setPreviousValue("");
+    setCurrentOperator("");
+  }
+
+  function deleteLastChar() {
+    let temp = currentValue;
+    temp = temp.slice(0, -1);
+    if (temp === "") {
+      temp = "0";
+      setCurrentValue(temp);
+    } else {
+      setCurrentValue(temp);
     }
   }
 
@@ -20,34 +44,68 @@ function App() {
           <input type="text" value={currentValue} readOnly />
         </div>
         <div>
-          <button className="basic-button">c</button>
-          <button className="basic-button">d</button>
-          <button className="basic-button">.</button>
-          <button className="basic-button">/</button>
+          <button className="basic-button" onClick={() => clear()}>
+            c
+          </button>
+          <button className="basic-button" onClick={() => deleteLastChar()}>
+            d
+          </button>
+          <button className="basic-button" onClick={() => onNumberClick(".")}>
+            .
+          </button>
+          <button className="basic-button" onClick={() => onOperatorClick("/")}>
+            /
+          </button>
         </div>
         <div>
           <button className="basic-button" onClick={() => onNumberClick("7")}>
             7
           </button>
-          <button className="basic-button" onClick={() => onNumberClick("8")}>8</button>
-          <button className="basic-button" onClick={() => onNumberClick("9")}>9</button>
-          <button className="basic-button">*</button>
+          <button className="basic-button" onClick={() => onNumberClick("8")}>
+            8
+          </button>
+          <button className="basic-button" onClick={() => onNumberClick("9")}>
+            9
+          </button>
+          <button className="basic-button" onClick={() => onOperatorClick("*")}>
+            *
+          </button>
         </div>
         <div>
-          <button className="basic-button" onClick={() => onNumberClick("4")}>4</button>
-          <button className="basic-button" onClick={() => onNumberClick("5")}>5</button>
-          <button className="basic-button" onClick={() => onNumberClick("6")}>6</button>
-          <button className="basic-button">-</button>
+          <button className="basic-button" onClick={() => onNumberClick("4")}>
+            4
+          </button>
+          <button className="basic-button" onClick={() => onNumberClick("5")}>
+            5
+          </button>
+          <button className="basic-button" onClick={() => onNumberClick("6")}>
+            6
+          </button>
+          <button className="basic-button" onClick={() => onOperatorClick("-")}>
+            -
+          </button>
         </div>
         <div>
-          <button className="basic-button" onClick={() => onNumberClick("1")}>1</button>
-          <button className="basic-button" onClick={() => onNumberClick("2")}>2</button>
-          <button className="basic-button" onClick={() => onNumberClick("3")}>3</button>
-          <button className="basic-button">+</button>
+          <button className="basic-button" onClick={() => onNumberClick("1")}>
+            1
+          </button>
+          <button className="basic-button" onClick={() => onNumberClick("2")}>
+            2
+          </button>
+          <button className="basic-button" onClick={() => onNumberClick("3")}>
+            3
+          </button>
+          <button className="basic-button" onClick={() => onOperatorClick("+")}>
+            +
+          </button>
         </div>
         <div>
-          <button className="basic-button" onClick={() => onNumberClick("7")}>0</button>
-          <button className="basic-button" onClick={() => onNumberClick("7")}>00</button>
+          <button className="basic-button" onClick={() => onNumberClick("0")}>
+            0
+          </button>
+          <button className="basic-button" onClick={() => onNumberClick("00")}>
+            00
+          </button>
           <button className="basic-button">=</button>
         </div>
       </div>
